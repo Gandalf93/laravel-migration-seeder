35acbf696d0ac91810978bcb7204a49e930a86fc
Giulia <giuliatullia93@gmail.com>
2021-09-15 00:35:33 +0200

    migration

---
+++ database/migrations/2021_09_14_200513_create_trips_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('title', 100);
+            $table->string('departure_place',100);
+            $table->string('destination', 100);
+            $table->date('departure_date');
+            $table->smallInteger('permanence_days')->unsigned();
+            $table->smallInteger('number_people')->unsigned();
+            $table->string('transport', 50);
+            $table->text('details');
+            $table->float('price', 8, 2);
+            $table->timestamps();
+            
+        });
+    }
+            
+            
+            
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
